518d0276fae5c90e4acc1fada998d26c44e6d1e7
RJ Van Hecke <vanheckeraoul@gmail.com>
2021-06-09 17:34:47 -0400

    finished open/close functionality of header sub-menus

--- js/header.js
+++ js/header.js
@@ -3,31 +3,29 @@
 function dropMenu() {
   const headerBtns = document.querySelectorAll('.header__nav-link');
 
-  function zeroItemHeight(item, index, arr) {
+  function hideItem(item, index, arr) {
     // Prevents the need for double-click on first menu drop
-    item.nextElementSibling.style.height = '0px';
+    item.nextElementSibling.style.display = 'none';
   }  // END zeroItemHeight DEFINITION
 
 
   function toggleOpenClose(item, index, arr) {
     // Adds event listeners to open/close header drop down menus
     item.addEventListener('click', () => {
-      if (item.nextElementSibling.style.height == '0px') {
+      if (item.nextElementSibling.style.display == 'none') {
         // Menu is closed, open it
-        item.nextElementSibling.style.height = 'inherit';
-        console.log('drop fired');
+        item.nextElementSibling.style.display = 'block';
 
       } else {
         // Menu is open, close it
-        item.nextElementSibling.style.height = '0px';
-        console.log('close fired');
+        item.nextElementSibling.style.display = 'none';
 
       }
     });
   }  // END toggleOpenClose DEFINITION
 
   // CODE RAN BY dropMenu
-  headerBtns.forEach(zeroItemHeight);
+  headerBtns.forEach(hideItem);
   headerBtns.forEach(toggleOpenClose);
 }
 
